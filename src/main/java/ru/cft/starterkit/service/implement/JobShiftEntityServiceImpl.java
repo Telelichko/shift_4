1b2e0acbb5494c04ce26498e06588ef8394a1c17
Telelichko <teleleichko@yandex.ru>
2019-07-04 15:00:55 +0700

    Классы и запросы работают

---
+++ src/main/java/ru/cft/starterkit/service/implement/JobShiftEntityServiceImpl.java
@@ -0,0 +1,45 @@
+package ru.cft.starterkit.service.implement;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.cft.starterkit.entity.JobShiftEntity;
+import ru.cft.starterkit.exception.ObjectNotFoundException;
+import ru.cft.starterkit.repository.JobShiftEntityRepository;
+import ru.cft.starterkit.service.JobShiftEntityService;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@Service
+public class JobShiftEntityServiceImpl implements JobShiftEntityService {
+
+    private final JobShiftEntityRepository jobShiftEntityRepository;
+
+    @Autowired
+    public JobShiftEntityServiceImpl(JobShiftEntityRepository jobShiftEntityRepository) {
+        this.jobShiftEntityRepository = jobShiftEntityRepository;
+    }
+
+//    @Override
+//    public JobShiftEntity add(String foo, Double bar) {
+//        return jobShiftEntityRepository.add(new JobShiftEntity(foo, bar, UUID.randomUUID()));
+//    }
+
+    @Override
+    public JobShiftEntity get(Long id) throws ObjectNotFoundException {
+        return jobShiftEntityRepository.get(id);
+    }
+
+    @Override
+    public Collection<JobShiftEntity> get() {
+        return jobShiftEntityRepository.get();
+    }
+
+    @Override
+    public JobShiftEntity add(JobShiftEntity jobShiftEntity) {
+        jobShiftEntity.setBaz(UUID.randomUUID());
+        return jobShiftEntityRepository.add(jobShiftEntity);
+    }
+
+}
